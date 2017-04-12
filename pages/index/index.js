e5706a146f3cf781a2f874c20b659dd5102e3512
xuyintao <xuyintao@126.com>
2017-04-12 15:20:42 +0800

    second

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,31 @@
 //index.js
 //获取应用实例
-var app = getApp()
+let app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    userInfo:{},
+    active:1,
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  navTap(e){
+    let navType=e.currentTarget.dataset.type;
+    switch(navType){
+      case "1":this.setData({active:1}); break;
+      case "2":this.setData({active:2}); break;
+      case "3":this.setData({active:3}); break;
+      case "4":this.setData({active:4}); break;
+      default:this.setData({active:1});
+    }
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
+    let that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
         userInfo:userInfo
       })
+      console.log(userInfo)
     })
   }
 })
